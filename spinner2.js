cbb099cd28d8aa2f9bdc415996366b87de0c7779
zainen <zainensuzuki@gmail.com>
2020-12-01 22:37:06 +0000

    same as previous solution

---
+++ spinner2.js
@@ -0,0 +1,30 @@
+// setTimeout(() => {
+//   process.stdout.write('\r|   ');
+// }, 100);
+
+// setTimeout(() => {
+//   process.stdout.write('\r/   ');
+// }, 300);
+
+// setTimeout(() => {
+//   process.stdout.write('\r-   ');
+// }, 500);
+
+// setTimeout(() => {
+//   // Need to escape the backslash since it's a special character.
+//   process.stdout.write('\r\\   ');
+// }, 700);
+
+// ... fill in the rest yourself ...
+let arr = ['|', '/', '-', '\\', '|', '/', '-', '\\', '|'];
+let x = 0;
+for (let line of arr) {
+  x += 200;
+  setTimeout(() => {
+    process.stdout.write('\r' + line);
+  }, x);
+}
+let finish = arr.length * 200;
+setTimeout(() => {
+  console.log();
+}, finish);
